9edbc4330826ee088df325b7c2c37326d526d636
t.i <isoittech@gmail.com>
2020-08-20 20:21:26 +0900

    lecture of jsx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+// class App extends Component {
+//   render() {
+//     return <div>Hello, World!! - with class</div>
+//   }
+// }
+
+// JSX
+// function App() {
+//   return (
+//     <div>Hello World!!!</div>
+//   );
+// }
+
+// NO JSX
+// function App() {
+//   return React.createElement(
+//     "div",
+//     null,
+//     "Hello, World - No jsx."
+//   )
+// }
 
 function App() {
   return (
-    <div><h1>Hello World!!!</h1></div>
+    <h1>Hello World!!!</h1>
   );
 }
 
